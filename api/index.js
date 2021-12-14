9c298116e56b1180a10b58b41c13465d1230001f
Jeremie-Allemand <jeremie.allemand@ceff.ch>
2021-12-14 15:28:19 +0000

    main

--- api/index.js
+++ api/index.js
@@ -3,13 +3,9 @@ const cors = require('cors')
 const app = express()
 const PORT = 8080
 
-const process_titi = (req,res) =>{
-    res.send("bonjour toto")
-}
-
 app.use(cors())
 app.use(express.json())
 
-// require('./routes/chat.route.js')(app)
+require('./routes/chat.route.js')(app)
 
 app.listen(PORT, () => console.log(`serveur à l'écoute sur le port ${PORT}`))
--- app/src/index.js
+++ app/src/index.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MainPage from './pages/MainPage';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {createStore} from 'redux'
+import { Provider } from 'react-redux';
+import reducer from './reducers/reducer'
+import './styles/index.css';
+
+const store = createStore(reducer)
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainPage/>}/>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
